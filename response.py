cce3187680a173d2dd507c5ef424cf6102d81382
fantazzzt <102978932+fantazzzt@users.noreply.github.com>
2023-06-03 21:19:20 -0500

    Update response.py
    Central timezone for bot and removing redundant calls

--- response.py
+++ response.py
@@ -1,10 +1,11 @@
 import requests
-from datetime import datetime
 import random
 import json
+import pytz
+import datetime
 
 
-apikey = "token"
+apikey = # click to set to your apikey
 limit = 10
 ckey = "My Project"  # set the client_key for the integration and use the same value for all API calls
 #Retrieves GIF from site
@@ -17,19 +18,47 @@ def get_gif(searchTerm):
     url = data["results"][gif_choice]["url"]
     return url
 
-
+def num_to_day(num):
+    if num == 0:
+        return "Monday"
+    if num == 1:
+        return "Tuesday"
+    if num == 2:
+        return "Wednesday"
+    if num == 3:
+        return "Thursday"
+    if num == 4:
+        return "Friday"
+    if num == 5:
+        return "Saturday"
+    if num == 6:
+        return "Sunday"
+days = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']
 def handle_response(message) -> str:
-    today = datetime.today().strftime('%A')
+    cst = pytz.timezone('US/Central')
+    cst_date = datetime.datetime.now(cst)
+    num = cst_date.weekday()
+    today = num_to_day(num)
     p_message = message.lower()
-    if 'day' in p_message:
+    if p_message in days:
+        pass
+    elif 'yesterday' in p_message:
+        num = (num - 1) % 7
+        today = num_to_day(num)
         return get_gif("it is asuka " + str(today) + " evangelion")
-    if 'today' in p_message:
+    elif 'tomorrow' in p_message:
+        num = (num + 1) % 7
+        today = num_to_day(num)
         return get_gif("it is asuka " + str(today) + " evangelion")
-    if 'good bot' in p_message :
+    elif 'day' in p_message:
+        return get_gif("it is asuka " + str(today) + " evangelion")
+    elif 'today' in p_message:
+        return get_gif("it is asuka " + str(today) + " evangelion")
+    elif 'good bot' in p_message :
         return get_gif("asuka love evangelion")
-    if 'bad bot' in p_message:
+    elif 'bad bot' in p_message:
         return get_gif("asuka mad evangelion")
-    if 'jackson' in p_message:
+    elif 'jackson' in p_message:
         return get_gif("jackson")
     else:
         pass
