6dbd8774493d774381ba62b7cf81654cef76a13e
Caden Massey <102978932+fantazzzt@users.noreply.github.com>
2023-07-30 12:49:29 -0500

    Update bot.py
    Remove commented out functions

--- bot.py
+++ bot.py
@@ -5,14 +5,6 @@ from datetime import datetime
 import pytz
 
 
-# async def send_message(message, user_message, is_private):
-#     try:
-#         respons = response.handle_response(user_message)
-#         await message.author.send(respons) if is_private else await message.channel.send(respons)
-#     except Exception as e:
-#         print(e)
-intents = discord.Intents.all()
-# WHEN = time(18, 0, 0)  # 6:00 PM
 bot = commands.Bot(command_prefix='.',intents=intents)
 
 channel_id =  # Put your channel id here
@@ -24,49 +16,6 @@ async def send_message_gif(message, user_message):
     except Exception as e:
         print(e)
 
-
-# async def called_once_a_day():  # Fired every day
-#     await bot.wait_until_ready()
-#     channel = bot.get_channel(channel_id)
-#     gif_url = response.handle_response('what day is it')
-#     await channel.send(gif_url)
-
-# async def background_task():
-#     now = datetime.utcnow()
-#     if now.time() > WHEN:  # Make sure loop doesn't start after {WHEN} as then it will send immediately the first time as negative seconds will make the sleep yield instantly
-#         tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
-#         seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
-#         await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start
-#     while True:
-#         now = datetime.utcnow() # You can do now() or a specific timezone if that matters, but I'll leave it with utcnow
-#         target_time = datetime.combine(now.date(), WHEN)  # 6:00 PM today (In UTC)
-#         seconds_until_target = (target_time - now).total_seconds()
-#         await asyncio.sleep(seconds_until_target)  # Sleep until we hit the target time
-#         await called_once_a_day()  # Call the helper function that sends the message
-#         tomorrow = datetime.combine(now.date() + timedelta(days=1), time(0))
-#         seconds = (tomorrow - now).total_seconds()  # Seconds until tomorrow (midnight)
-#         await asyncio.sleep(seconds)   # Sleep until tomorrow and then the loop will start a new iteration
-
-
-
-
-
-
-
-
-
-
-
-
-
-# COOLDOWN_AMOUNT = 180.0  # seconds
-# last_executed = time.time()
-# def assert_cooldown():
-#     global last_executed  # you can use a class for this if you wanted
-#     if last_executed + COOLDOWN_AMOUNT < time.time():
-#         last_executed = time.time()
-#         return True
-#     return False
 def get_time_until_launch():
     cst = pytz.timezone('US/Central')
     now = datetime.now(cst)
